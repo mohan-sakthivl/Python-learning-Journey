fa9208e16762c6968ed14c90041dcd2394712666
mohan-sakthivl <mohan.sakthivl@gmail.com>
2026-06-15 14:09:40 +0530

    Complete Day 3 if else programs

---
+++ Practice program/Day3/ifelse.py
@@ -0,0 +1,74 @@
+#Problem 1: Voting Eligibility
+age=int(input("Enter your age:"))
+if age>=18:
+    print("Your are Eligible to Vote")
+else:
+    print("Your are not Eligible to Vote")
+
+#Problem 2: Positive or Negative
+n=int(input("Enter a Number:"))
+if n>0:
+    print("Positive Number")
+elif n<0:
+    print("Negative Number")
+else:
+    print("Zero")
+
+#Problem 3: Even or Odd
+n=int(input("Enter the number:"))
+if n%2==0:
+    print("Even")
+else:
+    print("odd")
+
+#Problem 4: Pass or Fail
+mark=int(input("Enter your mark:"))
+if mark>=35:
+    print("pass")
+else:
+    print("fail")
+
+#Problem 5: Age Checker
+age=int(input("Enter your age: "))
+if age>=18:
+    print("Major")
+else:
+    print("Minor")
+
+#Problem 6: Largest of 2 Numbers
+n1=int(input("Enter your number:"))
+n2=int(input("Enter your number:"))
+if n1>n2:
+    print(f"{n1} is largest")
+else:
+    print(f"{n2} is largest")
+
+#Problem 7: Smallest of 2 Numbers
+n1=int(input("Enter your number:"))
+n2=int(input("Enter your number:"))
+if n1<n2:
+    print(f"{n1} is smallest")
+else:
+    print(f"{n2} is smallest")
+
+#Problem 8: Number is Zero or Not
+n=int(input("Enter your number:"))
+if n==0:
+    print("Number is zero")
+else:
+    print("Number is not zero")
+
+#Problem 9: Salary Eligibility
+salary=int(input("Enter your salary:"))
+if salary>=25000:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+#Problem 10: Password Check
+password=int(input("Enter your password:"))
+if password==1234:
+    print("Access Granted")
+else:
+    print("Access Denied")
+
---
+++ Practice program/Day3/testproblems.py
@@ -0,0 +1,59 @@
+#test1 Largest of 3 numbers
+n1=int(input("Enter the number n1:"))
+n2=int(input("Enter the number n2:"))
+n3=int(input("Enter the number n3:"))
+if n1>n2 and n1>n3:
+    print("N1 is largest")
+elif n2>n1 and n2>n3:
+    print("N2 is largest")
+else:
+    print("N3 is largest")
+
+#test2:Student Grade
+mark=int(input("Enter your mark:"))
+if mark>=90 and mark<=100:
+    print("A")
+elif mark>=80 and mark<=89:
+    print("B")
+elif mark>=70 and mark<=79:
+    print("C")
+else:
+    print("Fail")
+
+#test3:ATM Withdrawal Check
+savings=1500
+withdraw=int(input("Enter the amount:"))
+
+if withdraw <= savings:
+    print("Transaction Successful")
+else:
+    print("Insufficient Balance")
+balance =savings - withdraw
+print(f"Here is your balance amount:{balance}")
+
+#test4:Username Verification
+stored_username="mohan"
+name=input("enter your name:")
+if name==stored_username:
+    print("Verified")
+else:
+    print("Not verified")
+
+#test5:Smart Student Result System
+name=input("Enter your name:")
+mark=int(input("Enter your mark:"))
+print("-----Smart Student Result System-----")
+print(f"Name : {name}")
+print(f"Marks : {mark}")
+if mark>=90 and mark<=100:
+    print("Grade : A")
+    print("Result : Pass")
+elif mark>=80 and mark<=89:
+    print("Grade : B")
+    print("Result : Pass")
+elif mark>=70 and mark<=79:
+    print("Grade : C")
+    print("Result : Pass")
+else:
+    print("Grade : F")
+    print("Result : Fail")
